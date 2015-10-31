dd4c6bfe683a00dc115787ee20e25fddbc28eaee
Soundarya Thiagarajan <soundaryathiagarajan@gmail.com>
2015-10-31 20:40:45 +0530

    updated the javascript

--- drawBall.js
+++ drawBall.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById("bouncyBall"),
     ctx = canvas.getContext("2d"),
-    W = canvas.width, // simplified for demo
+    W = canvas.width, 
     H = canvas.height,
     gravity = .5,
     bounceFactor = .7;
@@ -27,8 +27,6 @@ Ball.prototype = {
 
   update: function() {
 
-  	//this.vx = W + this.x;
-	//this.vy = H + this.y;
 	this.x += this.ballDx;
 	this.y +=this.ballDy;
 	if(this.y > H - this.radius || this.y - this.radius < 0)
@@ -39,37 +37,7 @@ Ball.prototype = {
 	{
 		this.ballDx = -1 * this.ballDx;
 	}
-	//this.y = H + this.y;
-	//this.vx +=1;
-	//this.vy +=1;
-
-  	console.log(this.x);
-  	//console.log(this.y);
-  	
-  	//console.log(this.y);
-  	//
-
-    // this.x -=1;
-    // //this.vy += gravity;  // todo: limit bounce at some point or this value will be added
-    // if (this.y + this.radius > H) {
-    //   this.y = H - this.radius;
-    //   this.vy *= -1;
-    //   console.log(this.vy);
-    //   console.log(this.radius);
-    //   console.log(H);
-    //   console.log(W);
-    // }
-    // if (this.x + this.radius < W) {
-    //   this.x = W - this.radius;
-    //   //this.vx *= 1;
-    // }
-    // if(this.y > this.H - this.radius || this.y - this.radius < 0){
-    // 	this.y = H - this.radius;
-    //   	//this.vy *= -1;      	
-    // }
-    // else{
-    // 	this.y += this.vy;
-    // }
+  	console.log(this.x);  	
   }
 };
 
@@ -77,13 +45,12 @@ function clearCanvas() {
   ctx.clearRect(0, 0, W, H);
 }
 
-var balls = []; // define an array to hold the balls
-
+var balls = [];
 canvas.addEventListener('click', function(event) {
-  var rect = this.getBoundingClientRect(),  // adjust mouse position
+  var rect = this.getBoundingClientRect(),  
       x = event.clientX - rect.left,
       y = event.clientY - rect.top;
-  balls.push(new Ball(x, y));               // add a new instance
+  balls.push(new Ball(x, y));               
 });
 
 
@@ -91,8 +58,8 @@ canvas.addEventListener('click', function(event) {
   clearCanvas();
 
   for (var i = 0, ball; ball = balls[i]; i++) {
-    ball.draw(); // this will draw current ball
-    ball.update(); // this will update its position
+    ball.draw(); 
+    ball.update();
   }
 
   requestAnimationFrame(update);
